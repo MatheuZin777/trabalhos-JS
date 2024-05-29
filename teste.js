00274bd364a4f17168a1c545178e169bda53c6c2
MatheuZin777 <matheusxaviersousa38@gmail.com>
2024-05-29 11:46:38 -0300

    Add files via upload

---
+++ teste.js
@@ -0,0 +1,30 @@
+//shift + Alt + F
+var nome = 'Matheus'
+var email = 'matehusiasjia@gmail.com'
+var CPF = '0909090909'
+var DatadeNascimento = '06/03/2004'
+var sexo = 'Masculino'
+var endereço = 'Brasilia - DF'
+
+console.log('Bem-vindo,', nome, '\n',
+    'email', email, '\n',
+    'CPF:', CPF, '\n',
+    'Data de Nascimento:', DatadeNascimento, '\n',
+    'Sexo:', sexo, '\n',
+    'Endereço:', endereço)
+
+
+var w = 6
+var x = 8
+var y = 5
+var z = 3
+
+var div = (w + x + y + z) /4
+
+if (div >= 7) {
+    console.log('aprovado ')
+} else {
+    console.log('reprovado ')
+}
+
+
